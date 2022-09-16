2a9e91bbdd1ddb17fc1498c76b1732abd774e806
Mickylui <mickyluihk@icloud.com>
2022-09-16 15:02:01 +0800

    finished: private route in app.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from "react";
-// import logo from './logo.svg';
-import "./App.css";
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
-
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "./Redux/store";
+import "./App.css";
+
 import { LogIn, SignUp } from "./Pages/Account";
 import { ProductDetail } from "./Pages/Product/ProductDetail";
 import { Products } from "./Pages/Product/Products";
@@ -12,43 +11,46 @@ import LandingPage from "./Pages/LandingPage";
 import SocialPlatform from "./Pages/Platform/SocialPlatform";
 import Profile from "./Pages/User/Profile";
 import AdminProfile from "./Pages/Admin/AdminProfile";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "./Redux/store";
-import { getUserDataJWTFetch } from "./Api/AccountFetch";
-import { Slideshow } from "./Components/AutoSlider";
 import CompleteForm from "./Pages/User/CompleteForm";
 import ReportUser from "./Pages/User/ReportUser";
-import Table from "./Components/Table";
 
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import { Slideshow } from "./Components/AutoSlider";
+import Table from "./Components/Table";
+import PrivateRoute from "./Components/PrivateRoute";
+
+import { getUserDataJWTFetch } from "./Api/AccountFetch";
 
 function App() {
     const isLoggedIn = useSelector((state: RootState) => state.account.isLoggedIn);
     const dispatch: AppDispatch = useDispatch();
-    // if has JWT -> backend check if valid -> status: login -> true, isAdmin change
     useEffect(() => {
         GetUserDataJWT();
     });
     const GetUserDataJWT = async () => {
         const token = window.localStorage.getItem("token");
         if (!isLoggedIn && token) {
-            // console.log("GetUserDataJWT:", token);
             const userDataJWT = await dispatch(getUserDataJWTFetch({ token }));
             console.log("userDataJWT:", userDataJWT);
         }
-        // navigate("/")
     };
     return (
         <div className="App">
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={<LandingPage />} />
+                    <Route index element={<LandingPage />} />
                     <Route path="logIn" element={<LogIn />} />
                     <Route path="signUp" element={<SignUp />} />
-                    <Route path="user" element={<Profile />} />
-                    <Route path="user/form" element={<CompleteForm />} />
-                    <Route path="user/report" element={<ReportUser />} />
-                    <Route path="admin" element={<Profile />} />
+                    <Route path="user" element={<PrivateRoute />}>
+                        <Route path="profile" element={<Profile />} />
+                        <Route path="profile/form" element={<CompleteForm />} />
+                        <Route path="profile/report" element={<ReportUser />} />
+                    </Route>
+                    <Route path="admin" element={<PrivateRoute />}>
+                        <Route path="manage" element={<Profile />} />Í
+                    </Route>
                     <Route path="products" element={<Products />} />
                     <Route path="productDetail" element={<ProductDetail />} />
                     <Route path="posts" element={<SocialPlatform />} />
