1b72e227f4d2bd3e103aa0fc70ce7db444659f92
JD <jade.k919@gmail.com>
2022-09-17 18:26:01 +0800

    added userProfile settings

--- my-app/src/Pages/User/Settings.tsx
+++ my-app/src/Pages/User/Settings.tsx
@@ -1,5 +1,6 @@
-import { Box, FormControl, FormLabel } from '@chakra-ui/react';
-import React from 'react';
+import { Box, FormControl, FormLabel, Input, HStack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { ImageUpload } from "../../Components/ImageUpload";
 
 function Settings() {
     const userProfile = {
@@ -13,18 +14,77 @@ function Settings() {
         password: "1234",
         confirm_password: "",
         address: "New Territories, Tsuen Wan West, One Town",
-    }
+        contact: "123456",
+    };
+    const [userName, setUsername] = useState("");
 
     return (
         <div>
-               <Box p="2rem" borderWidth="1px" borderRadius="lg" overflow="hidden" m="4rem">
-                <Box>Settings</Box>
-                <Box>
-                    <FormControl>
-                        <FormLabel>Username</FormLabel>
-                    </FormControl>
-                </Box>
-               </Box>
+            <Box p="2rem" borderWidth="1px" borderRadius="lg" overflow="hidden" m="4rem">
+                <Box mb="2rem" fontWeight='bold' fontSize='2rem'>Settings</Box>
+
+                <FormControl>
+                    <HStack spacing="2rem">
+                        <FormLabel>Username:</FormLabel>
+                        <Input type="string" placeholder={userProfile.userName}></Input>
+                    </HStack>
+                </FormControl>
+                <FormControl>
+                    <HStack spacing="2rem">
+                        <FormLabel>First Name:</FormLabel>
+                        <Input type="string" placeholder={userProfile.firstName}></Input>
+                    </HStack>
+                </FormControl>
+                <FormControl>
+                    <HStack spacing="2rem">
+                        <FormLabel>Last Name:</FormLabel>
+                        <Input type="string" placeholder={userProfile.lastName}></Input>
+                    </HStack>
+                </FormControl>
+                <FormControl>
+                    <HStack spacing="2rem">
+                        <FormLabel>Bio:</FormLabel>
+                        <Input type="string" placeholder={userProfile.bio}></Input>
+                    </HStack>
+                </FormControl>
+                <FormControl>
+                    <HStack spacing="2rem">
+                        <FormLabel>Email:</FormLabel>
+                        <Input type="string" placeholder={userProfile.email}></Input>
+                    </HStack>
+                </FormControl>
+                <FormControl>
+                    <HStack spacing="2rem">
+                        <FormLabel>Password:</FormLabel>
+                        <Input type="string" placeholder={userProfile.password}></Input>
+                    </HStack>
+                </FormControl>
+                <FormControl>
+                    <HStack spacing="2rem">
+                        <FormLabel>Confirm Password</FormLabel>
+                        <Input type="string" placeholder={userProfile.password}></Input>
+                    </HStack>
+                </FormControl>
+                <FormControl>
+                    <HStack spacing="2rem">
+                        <FormLabel>Address</FormLabel>
+                        <Input type="string" placeholder={userProfile.address}></Input>
+                    </HStack>
+                </FormControl>
+                <FormControl>
+                    <HStack spacing="2rem">
+                        <FormLabel>Contact</FormLabel>
+                        <Input type="string" placeholder={userProfile.contact}></Input>
+                    </HStack>
+                </FormControl>
+                <FormControl>
+                    <HStack>
+                        <FormLabel>Profile Image:</FormLabel>
+                    <ImageUpload />
+                    </HStack>
+                    
+                </FormControl>
+            </Box>
         </div>
     );
 }
