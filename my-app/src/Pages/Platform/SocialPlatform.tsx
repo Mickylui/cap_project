8a16d02ff21ef5e0422f9a8297e85c2156691c9e
JD <jade.k919@gmail.com>
2022-09-17 14:06:32 +0800

    added scrollToTopButton

--- my-app/src/Pages/Platform/SocialPlatform.tsx
+++ my-app/src/Pages/Platform/SocialPlatform.tsx
@@ -7,16 +7,92 @@ import React from "react";
 import PostForm from "./PostForm";
 import { Link } from "react-router-dom";
 import { WarningTwoIcon } from "@chakra-ui/icons";
+import ScrollToTopButton from "../../Components/ScrollToTopButton";
 
 function SocialPlatform() {
-    const postItem = {
+    const postItems = [
+        {
+        id: 1,
         imageUrl: "./SkateBoardLogo.png",
         imageAlt: "SkateBoardLogo",
         title: "Fancy Style Design",
         description: "My 1st design when I was 15",
         userName: "Jason",
         numLikes: 190,
-    };
+    },
+        {
+        id: 2,
+        imageUrl: "./SkateBoardLogo.png",
+        imageAlt: "SkateBoardLogo",
+        title: "Fancy Style Design",
+        description: "My 1st design when I was 15",
+        userName: "Jason",
+        numLikes: 190,
+    },
+        {
+        id: 3,
+        imageUrl: "./SkateBoardLogo.png",
+        imageAlt: "SkateBoardLogo",
+        title: "Fancy Style Design",
+        description: "My 1st design when I was 15",
+        userName: "Jason",
+        numLikes: 190,
+    },
+        {
+        id: 4,
+        imageUrl: "./SkateBoardLogo.png",
+        imageAlt: "SkateBoardLogo",
+        title: "Fancy Style Design",
+        description: "My 1st design when I was 15",
+        userName: "Jason",
+        numLikes: 190,
+    },
+        {
+        id: 5,
+        imageUrl: "./SkateBoardLogo.png",
+        imageAlt: "SkateBoardLogo",
+        title: "Fancy Style Design",
+        description: "My 1st design when I was 15",
+        userName: "Jason",
+        numLikes: 190,
+    },
+        {
+        id: 6,
+        imageUrl: "./SkateBoardLogo.png",
+        imageAlt: "SkateBoardLogo",
+        title: "Fancy Style Design",
+        description: "My 1st design when I was 15",
+        userName: "Jason",
+        numLikes: 190,
+    },
+        {
+        id: 4,
+        imageUrl: "./SkateBoardLogo.png",
+        imageAlt: "SkateBoardLogo",
+        title: "Fancy Style Design",
+        description: "My 1st design when I was 15",
+        userName: "Jason",
+        numLikes: 190,
+    },
+        {
+        id: 5,
+        imageUrl: "./SkateBoardLogo.png",
+        imageAlt: "SkateBoardLogo",
+        title: "Fancy Style Design",
+        description: "My 1st design when I was 15",
+        userName: "Jason",
+        numLikes: 190,
+    },
+        {
+        id: 6,
+        imageUrl: "./SkateBoardLogo.png",
+        imageAlt: "SkateBoardLogo",
+        title: "Fancy Style Design",
+        description: "My 1st design when I was 15",
+        userName: "Jason",
+        numLikes: 190,
+    },
+]
     return (
         <div>
             <Input
@@ -43,7 +119,9 @@ function SocialPlatform() {
             </HStack>
 
             <SimpleGrid columns={[2, null, 3]} spacing="40px" margin="5rem">
-                <RouteLink to="postDetail">
+                {postItems.map((postItem)=> 
+                (
+                    <RouteLink to="postDetail">
                     <Box maxW="sm" borderRadius="lg" overflow="hidden">
                         <Image
                             src={postItem.imageUrl}
@@ -83,63 +161,9 @@ function SocialPlatform() {
                         </RouteLink>
                     </Box>
                 </RouteLink>
-
-                <Box maxW="sm" borderRadius="lg" overflow="hidden">
-                    <Image
-                        src={postItem.imageUrl}
-                        alt={postItem.imageAlt}
-                        border="1px"
-                        borderRadius="lg"
-                    />
-
-                    <Box p="6">
-                        <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" noOfLines={1}>
-                            {postItem.title}
-                        </Box>
-
-                        <Box>{postItem.description}</Box>
-                    </Box>
-                    <Tag size="lg" colorScheme="none" borderRadius="full">
-                        <Avatar
-                            src="https://bit.ly/sage-adebayo"
-                            size="md"
-                            name="Segun Adebayo"
-                            ml={-1}
-                            mr={2}
-                        />
-                        <TagLabel>{postItem.userName}</TagLabel> <FaHeart color="red" />{" "}
-                        {postItem.numLikes}
-                    </Tag>
-                </Box>
-
-                <Box maxW="sm" borderRadius="lg" overflow="hidden">
-                    <Image
-                        src={postItem.imageUrl}
-                        alt={postItem.imageAlt}
-                        border="1px"
-                        borderRadius="lg"
-                    />
-
-                    <Box p="6">
-                        <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" noOfLines={1}>
-                            {postItem.title}
-                        </Box>
-
-                        <Box>{postItem.description}</Box>
-                    </Box>
-                    <Tag size="lg" colorScheme="none" borderRadius="full">
-                        <Avatar
-                            src="https://bit.ly/sage-adebayo"
-                            size="md"
-                            name="Segun Adebayo"
-                            ml={-1}
-                            mr={2}
-                        />
-                        <TagLabel>{postItem.userName}</TagLabel> <FaHeart color="red" />{" "}
-                        {postItem.numLikes}
-                    </Tag>
-                </Box>
+                ))}
             </SimpleGrid>
+            <ScrollToTopButton />
         </div>
     );
 }
