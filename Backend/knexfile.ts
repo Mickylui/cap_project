c74abc05d12658b85e72c46cc2de6a869e7d7116
Mickylui <mickyluihk@icloud.com>
2022-09-25 21:34:35 +0800

    trying cicd

--- Backend/knexfile.ts
+++ Backend/knexfile.ts
@@ -12,15 +12,29 @@ const config: { [key: string]: Knex.Config } = {
             user: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
         },
+        pool: {
+            min: 2,
+            max: 10,
+          },
+          migrations: {
+            tableName: "knex_migrations",
+          },
     },
 
-    testing: {
+    test: {
         client: "postgresql",
         connection: {
             database: process.env.DB_NAME,
             user: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
         },
+        pool: {
+            min: 2,
+            max: 10,
+          },
+          migrations: {
+            tableName: "knex_migrations",
+          },
     },
 
     production: {
