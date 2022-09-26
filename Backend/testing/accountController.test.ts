f28893f303ca5ac14b66b13809c815ee6a35d00c
Mickylui <mickyluihk@icloud.com>
2022-09-26 12:37:43 +0800

    doing accountController.test.ts

--- Backend/testing/accountController.test.ts
+++ Backend/testing/accountController.test.ts
@@ -2,8 +2,12 @@ import { AccountService } from "../services/accountService";
 import { AccountController } from "../controllers/accountController";
 import type { Knex } from "knex";
 import type { Request, Response } from "express";
+import jwtSimple from "jwt-simple";
+import { checkPassword } from "../utils/hash";
 
 jest.mock("../services/accountService");
+jest.mock("jwt-simple");
+jest.mock("../utils/hash");
 
 describe("accountService", () => {
     let service: AccountService;
@@ -13,7 +17,8 @@ describe("accountService", () => {
 
     beforeEach(() => {
         service = new AccountService({} as Knex);
-        service.signUp = jest.fn();
+        service.logIn = jest.fn(() => Promise.resolve({ success: true, message: "" }));
+
         req = {
             params: {},
             query: {},
@@ -24,18 +29,20 @@ describe("accountService", () => {
             json: jest.fn(),
         } as any as Response;
 
+        (checkPassword as jest.Mock).mockResolvedValue(true);
         controller = new AccountController(service);
     });
 
-    it("signUp - success", async () => {
-        await controller.signUp(req, res);
-        expect(service.signUp).toBeCalled();
-        expect(res.json).toBeCalledWith({ success: true });
+    it("logIn - missing password", async () => {
+        req.body = { email: "admin@1.com" };
+        await controller.logIn(req, res);
+        expect(service.logIn).toBeCalled();
+        expect(res.json).toBeCalledWith({ success: false, message: "Invalid password" });
     });
 
-    it("signUp - failed with error from service", async () => {
-        await controller.signUp(req, res);
-        expect(service.signUp).toBeCalled();
-        expect(res.json).toBeCalledWith({ success: false, message: "internal server error" });
-    });
+    // it("logIn - failed with error from service", async () => {
+    //     await controller.logIn(req, res);
+    //     expect(service.logIn).toBeCalled();
+    //     expect(res.json).toBeCalledWith({ success: false, message: "internal server error" });
+    // });
 });
